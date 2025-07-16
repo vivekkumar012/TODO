a36a6c688a58b6ee5db84bea7bf48ac219cc06ac
Vivek Kumar <vh118854@gmail.com>
2025-07-17 02:50:21 +0530

    project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+      <div className='bg-gradient-to-r from-black to-blue-950 h-screen'>
+        <div className='text-2xl font-bold text-yellow-800 bg-gray-600'>
+        Hi There how are you
+      </div>
+      </div>
+  )
+}
+
+export default App
