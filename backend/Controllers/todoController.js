f79d82d49c8e5d3eb6e0cfbb50f0623c3ce14e2b
Vivek Kumar <vh118854@gmail.com>
2025-07-21 23:28:31 +0530

    todo start

--- backend/Controllers/todoController.js
+++ backend/Controllers/todoController.js
@@ -1,22 +1,7 @@
-import express from 'express';
+export const createTodo = async(req, res) => {}
 
-export const createTodo = async (req, res) => {
-    try {
-        
-    } catch (error) {
-        
-    }
-}
+export const getTodo = async(req, res) => {}
 
-export const getTodo = async (req, res) => {
-    
-}
+export const updateTodo = async(req, res) => {}
 
-
-export const updateTodo = async (req, res) => {
-
-}
-
-export const deleteTodo = async (req, res) => [
-
-]
+export const deleteTodo = async(req, res) => {}
